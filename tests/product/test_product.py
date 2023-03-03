20ff2ceb1980fff957c1a529c9e140f127625e30
Vinícius Gabriel <viniciusgsa99@gmail.com>
2023-03-03 15:43:46 -0300

    Requisito 1 concluído. Implementando testes da classe Product.

--- tests/product/test_product.py
+++ tests/product/test_product.py
@@ -1,5 +1,21 @@
-# from inventory_report.inventory.product import Product
+from inventory_report.inventory.product import Product
 
 
 def test_cria_produto():
-    pass  # Seu teste deve ser escrito aqui
+    product = Product(
+        id=1234,
+        nome_do_produto='Produto de teste',
+        nome_da_empresa='Empresa de teste',
+        data_de_fabricacao='03/03/23',
+        data_de_validade='03/03/25',
+        numero_de_serie='256',
+        instrucoes_de_armazenamento='Instrucao de teste'
+    )
+
+    assert product.id == 1234
+    assert product.nome_do_produto == 'Produto de teste'
+    assert product.nome_da_empresa == 'Empresa de teste'
+    assert product.data_de_fabricacao == '03/03/23'
+    assert product.data_de_validade == '03/03/25'
+    assert product.numero_de_serie == '256'
+    assert product.instrucoes_de_armazenamento == 'Instrucao de teste'
