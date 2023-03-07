5bb1a133874c276395072594924b39e23ef424ba
Vinícius Gabriel <viniciusgsa99@gmail.com>
2023-03-07 17:49:44 -0300

    Requisito 3 concluído. Implementando sobrescrita do método generate na classe CompleteReport herdada da classe SimpleReport.

--- inventory_report/reports/complete_report.py
+++ inventory_report/reports/complete_report.py
@@ -1,2 +1,26 @@
-class CompleteReport:
-    pass
+from .simple_report import SimpleReport
+
+
+class CompleteReport(SimpleReport):
+    @staticmethod
+    def generate(relatory_date):
+        simple_report = SimpleReport.generate(relatory_date)
+
+        companies = {}
+
+        for company in relatory_date:
+            if company['nome_da_empresa'] in companies:
+                companies[company['nome_da_empresa']] += 1
+            else:
+                companies[company['nome_da_empresa']] = 1
+
+        total_products = ""
+
+        for company in relatory_date:
+            total_products += f"- {company['nome_da_empresa']}: {companies[company['nome_da_empresa']]}\n"
+
+        return (
+            f"{simple_report}\n"
+            "Produtos estocados por empresa:\n"
+            f"{total_products}"
+        )
