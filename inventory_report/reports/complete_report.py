38771812d0a66ccc60b70802d513b5402f2be2ba
Vinícius Gabriel <viniciusgsa99@gmail.com>
2023-03-07 22:00:27 -0300

    Corrigindo bug no github.

--- inventory_report/reports/complete_report.py
+++ inventory_report/reports/complete_report.py
@@ -14,14 +14,14 @@ class CompleteReport(SimpleReport):
             else:
                 comps[company['nome_da_empresa']] = 1
 
-        total_products = ""
+        total_products_company = ""
 
         for cp in relatory_date:
-            total_products += \
+            total_products_company += \
                 f"- {cp['nome_da_empresa']}: {comps[cp['nome_da_empresa']]}\n"
 
         return (
             f"{simple_report}\n"
             "Produtos estocados por empresa:\n"
-            f"{total_products}"
+            f"{total_products_company}"
         )
