545e807a70018a10123ab03a121e3a6ea4c8bb06
Vinícius Gabriel <viniciusgsa99@gmail.com>
2023-03-09 10:29:06 -0300

    Requisito 5 concluído. Refatorando método import_data e implementando função para ler arquivos .json.

--- inventory_report/inventory/inventory.py
+++ inventory_report/inventory/inventory.py
@@ -1,17 +1,40 @@
 from inventory_report.reports.simple_report import SimpleReport
 from inventory_report.reports.complete_report import CompleteReport
 import csv
+import json
 
 
 class Inventory:
     @staticmethod
-    def import_data(file, type):
-        with open(file, encoding='utf-8') as file:
-            file_content = csv.DictReader(file, delimiter=',', quotechar='"')
-            file_content_edited = [row for row in file_content]
-            if type == 'simples':
-                report_simple = SimpleReport.generate(file_content_edited)
-                return report_simple
-            elif type == 'completo':
-                report_coomplete = CompleteReport.generate(file_content_edited)
-                return report_coomplete
+    def json_converter(path):
+        try:
+            with open(path, 'r') as file:
+                return [row for row in json.load(file)]
+        except FileNotFoundError:
+            raise ("File not found")
+
+    @staticmethod
+    def csv_converter(path):
+        try:
+            with open(path, 'r', encoding='utf-8') as file:
+                return [row for row in
+                        csv.DictReader(file, delimiter=',', quotechar='"')]
+        except FileNotFoundError:
+            raise ("File not found")
+
+    @classmethod
+    def extensions_type(cls, path):
+        if '.csv' in path:
+            return cls.csv_converter(path)
+        if '.json' in path:
+            return cls.json_converter(path)
+        if '.xml' in path:
+            return cls.json_converter(path)
+
+    @classmethod
+    def import_data(cls, path, type):
+        file_content = cls.extensions_type(path=path)
+        if type == 'simples':
+            return SimpleReport.generate(file_content)
+
+        return CompleteReport.generate(file_content)
