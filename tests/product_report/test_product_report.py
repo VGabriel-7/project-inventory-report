1d71e8c012487b6511b68e66fb6ac595e95e7dae
Vinícius Gabriel <viniciusgsa99@gmail.com>
2023-03-09 19:03:05 -0300

    Requisito 8 concluído. Implementando teste para a classe Product.

--- tests/product_report/test_product_report.py
+++ tests/product_report/test_product_report.py
@@ -1,5 +1,20 @@
-# from inventory_report.inventory.product import Product
+from inventory_report.inventory.product import Product
 
 
 def test_relatorio_produto():
-    pass  # Seu teste deve ser escrito aqui
+    expect_string = """O produto test fabricado em \
+2022-03-22 por empresa test com \
+validade até 2023-03-22 precisa ser \
+armazenado Armazenamento test."""
+
+    report = Product(
+                1,
+                'test',
+                'empresa test',
+                '2022-03-22',
+                '2023-03-22',
+                'PKG87',
+                'Armazenamento test',
+            )
+
+    assert str(report) == expect_string
